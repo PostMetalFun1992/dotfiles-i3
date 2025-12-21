1c311f6ea80b4467adeefbe9a45fd22f4f9eaf19
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2025-12-21 14:09:53 +0400

    Added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,32 @@
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -gx EDITOR helix
+set -gx VISUAL helix
+
+if not status is-interactive
+    return
+end
+
+set -g fish_greeting
+
+starship init fish | source
+
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons -l --group-directories-first"
+alias la="eza --icons -la --group-directories-first"
+alias tree="eza --icons --tree"
+
+alias cat="bat"
+
+alias top="btop"
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git log"
+
+alias k="kubectl"
+alias hx="helix"
+alias zj="zellij"
+
+alias i3conf="nvim ~/.config/i3/config"
